01e448356a00b209964124bd367df0c856a04e42
kensuke sakai <spiknsk@gmail.com>
2016-04-12 20:24:14 +0900

    UnityGameUI_SampleData追加

---
+++ Assets/Chapter2/WorldSpace/Assets/PlayerMotion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotion : MonoBehaviour
+{
+	public float duration = 0.4f;// 移動にかかる時間（秒）
+
+	// プレイヤーをX軸方向にdistanceの距離だけ移動させる
+	public void MoveX (float distance)
+	{
+		StartCoroutine(MoveXCoroutine(distance));
+	}
+
+	IEnumerator MoveXCoroutine(float distance)
+	{
+		float currentX = transform.position.x;
+		float targetX = currentX + distance;
+		float startTime = Time.time;
+		float t = 0;
+
+		while(t < 1.0f)
+		{
+			t = (Time.time - startTime) / duration;
+			transform.position = new Vector3(Mathf.SmoothStep(currentX, targetX, t), 0, 0);
+			yield return 0;
+		}
+	}
+}
---
+++ Assets/Chapter4/RouletteBattle/Assets/Scripts/Battle/Confetti.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//勝利時に出てくるクラッカーのパーティクル演出のソートオーダーを変更
+public class Confetti : MonoBehaviour
+{
+	public string sortingLayerName; 	//デフォルトは空で良い
+	public int sortingOrder;			//ソートオーダー
+
+	void Start ()
+	{
+		Renderer renderer = this.GetComponent<Renderer>();
+		renderer.sortingLayerName = sortingLayerName;
+		renderer.sortingOrder = sortingOrder;
+	}
+}
